a92e08befb40a85154cbc085f374a43ad0be413b
matthewzring <matthewzring@gmail.com>
2026-02-09 18:55:50 -0600

    update license

--- src/logging/Logger.cs
+++ src/logging/Logger.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2023-2025 Matthew Ring
+ * Copyright 2023-2026 Matthew Ring
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
